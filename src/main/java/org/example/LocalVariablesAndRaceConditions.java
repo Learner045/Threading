88651edf533303ba634274a48c597820b5393d3f
Learner045 <sdesai12@binghamton.edu>
2024-01-10 21:42:19 -0500

    Th- Race condition and parallel processing of text file

---
+++ src/main/java/org/example/LocalVariablesAndRaceConditions.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class LocalVariablesAndRaceConditions {
+
+    // global variables or class-level variables, allocated on heap
+    private static int globalCounter = 0;
+
+    /* ThreadLocal is a wrapper to a value and will be private to each thread even though it is a class field.
+     * We can also provide an initial value ThreadLocal.withInitial(()-> "initValue");
+     */
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+
+    static void localVarsAndRaceConditions() {
+        Thread t1 = new Thread(new MyThread());
+        Thread t2 = new Thread(new MyThread());
+
+        t1.start();
+        t2.start();
+    }
+
+    static class MyThread implements Runnable {
+        @Override
+        public void run() {
+            // private variables for a thread which would reside in thread's stack
+            int counter = 0;
+            threadLocal.set("myValue"); // private to each thread
+        }
+    }
+}
